45f5172af51d7ab9de2cae7b21807ce0ffcc40ef
bujosa <davidbujosa@gmail.com>
2021-01-01 03:02:25 -0400

    Added new interfaces

--- src/blue/blue.module.ts
+++ src/blue/blue.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlueController } from './blue.controller';
 import { BlueService } from './blue.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/blue',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
+  ],
   controllers: [BlueController],
   providers: [BlueService],
 })
