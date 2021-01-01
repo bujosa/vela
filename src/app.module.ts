b63679fde557ccf9d4342f04c619d0f8698ae53c
bujosa <davidbujosa@gmail.com>
2021-01-01 02:03:58 -0400

    Eliminated bad files and create blue module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlueModule } from './blue/blue.module';
 
 @Module({
-  imports: [],
+  imports: [BlueModule],
   controllers: [AppController],
   providers: [AppService],
 })
